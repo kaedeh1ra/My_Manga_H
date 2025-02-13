2d2a9bfaada77e1393dba7cd8c4b3ea69452bf58
Денис <denyar3100@gmail.com>
2025-02-14 01:57:00 +0400

    это пизд

---
+++ lib/screens/drawing_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({super.key});
+
+  @override
+  State<DrawingScreen> createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
