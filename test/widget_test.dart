3de11997526e4ac2759b7d366019527ceea4f70d
genazavr <hestorikal@mail.ru>
2025-02-15 15:19:55 +0400

    денис Гы гы x1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,13 +13,13 @@ import 'package:my_manga_h/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PageViewExampleApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
